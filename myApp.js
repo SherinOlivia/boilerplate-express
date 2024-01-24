05191cd75669de9e70ef12aade8b5d04d1928d18
Sherin Olivia <sochronicle@gmail.com>
2024-01-24 10:27:20 +0700

    lesson 1: hello world console

--- myApp.js
+++ myApp.js
@@ -2,12 +2,11 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res) {
-    console.log('Hello World');
+    console.log('"Hello World"');
     res.send('Hello World');
   });
 
-
-
+console.log('"Hello World"');
 
 
 
